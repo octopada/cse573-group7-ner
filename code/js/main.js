53146032c8138c896a0efc60f609b92e26504986
octopada <gogollian@gmail.com>
2022-04-12 18:56:31 -0700

    Implement tag highlighting

--- code/js/main.js
+++ code/js/main.js
@@ -1,4 +1,24 @@
+let svg;
+let width, height;
+
+let baseX = 16;
+let baseY = 48;
+let currX = 16;
+let currY = 48;
+let wordCount = 0;
+
+document.addEventListener('DOMContentLoaded', function () {
+    svg = d3.select("#classifiedTextSVG");
+    width = +svg.style('width').replace('px', '');
+    height = +svg.style('height').replace('px', '');
+});
+
 function nerClassify() {
+    currX = baseX;
+    currY = baseY;
+    svg.selectAll('*').remove();
+    wordCount = 0;
+
     let textArea = document.getElementById("classifyTexts");
     let modelSelect = document.getElementById("modelSelect");
     let modSelect = document.getElementById("modSelect");
@@ -23,5 +43,105 @@ function nerClassify() {
         }).then(function (data) {
         console.log('POST response:');
         console.log(data['result']);
+
+        let taggedLocatedWords = []
+        let previousLetterCount = 0;
+        for (let idx in data['result']) {
+            let taggedWord = data['result'][idx];
+
+            let location = getWordLocation(previousLetterCount)
+
+            let taggedLocatedWord = {}
+            taggedLocatedWord.word = taggedWord['word'];
+            taggedLocatedWord.tag = taggedWord['tag'];
+            taggedLocatedWord.x = location.x;
+            taggedLocatedWord.y = location.y;
+
+            taggedLocatedWords.push(taggedLocatedWord);
+
+            // console.log("appending");
+            // svg.append('text')
+            //     .attr('x', location.x)
+            //     .attr('y', location.y)
+            //     .style('font', '48px courier')
+            //     .style("fill", "blue")
+            //     .text(taggedWord['word']);
+
+            wordCount++;
+            previousLetterCount = taggedWord['word'].length;
+        }
+
+        console.log(taggedLocatedWords);
+
+        svg.append('g')
+            .selectAll('rect')
+            .data(taggedLocatedWords)
+            .join('rect')
+            .attr('x', d => d.x)
+            .attr('y', d => d.y)
+            .style('fill', d => getHighlightColor(d.tag))
+            .style('opacity', '1');
+
+        svg.append('g')
+            .style('font', '48px courier')
+            .style('fill', 'black')
+            .selectAll('text')
+            .data(taggedLocatedWords)
+            .join('text')
+            .attr('x', d => d.x)
+            .attr('y', d => d.y)
+            .text(d => d.word);
+
+        svg.selectAll("text")
+            .each(function (d) {
+                d.bbox = this.getBBox();
+            });
+
+        const xMargin = 4
+        const yMargin = 0
+        svg.selectAll("rect")
+            .data(taggedLocatedWords)
+            .join("rect")
+            .attr("width", d => d.bbox.width + 2 * xMargin)
+            .attr("height", d => d.bbox.height + 2 * yMargin)
+            .attr('transform', function (d) {
+                return `translate(-${xMargin}, -${d.bbox.height * 0.8 + yMargin})`
+            });
     });
+}
+
+function getHighlightColor(tag) {
+    switch (tag) {
+        case 'PER':
+            return '#F2BAC9';
+        case 'ORG':
+            return '#BAD7F2';
+        case 'LOC':
+            return '#F2E2BA';
+        case 'MISC':
+            return '#B0F2B4';
+        case 'O':
+            return 'white';
+    }
+}
+
+function getWordLocation(previousLetterCount) {
+    let location = {
+        x: currX,
+        y: currY
+    };
+
+    if (wordCount === 0) {
+        return location;
+    }
+
+    currX += (28 * previousLetterCount) + 20;
+    if (currX > 1400) {
+        currX = baseX;
+        currY += (currY + 12);
+    }
+
+    location.x = currX;
+    location.y = currY;
+    return location;
 }
